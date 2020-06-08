eeda0dee950fd154d7df1bebf2db4b836f5df0d0
Thiago Naves <naves.thiago@gmail.com>
2020-06-08 01:14:00 -0300

    Started working on a higher level tree api

--- mainwindow.py
+++ mainwindow.py
@@ -14,13 +14,14 @@ class MainWindow:
         self.edit = self.window.edit
         self.label = self.window.label
 
+        self.loaded = {} # Lists loaded directories and files
+
     def show(self):
         self.window.show()
 
     def btnTopClick(self):
-        item = QtWidgets.QTreeWidgetItem([self.edit.text(), "foo"], 1)
-        self.fileTree.insertTopLevelItem(0, item)
-        self.label.setText(self.edit.text())
+        self.addTopEntry(self.edit.text(), 123, True)
+        #self.label.setText(self.edit.text())
 
     def btnChildClick(self):
         sel = self.fileTree.currentItem()
@@ -29,12 +30,33 @@ class MainWindow:
             sel.addChild(item)
 
     def fileTreeItemExpanded(self, item):
+        self.label.setText(self._entryPath(item))
+
+    def _entryPath(self, item):
         path = "/" + item.text(0)
         parent = item.parent()
         while parent:
             path = "/" + parent.text(0) + path
             parent = parent.parent()
-        self.label.setText(path)
+        return path
+
+    def _createDirectoryEntry(self, parent, name):
+        ''' Creates a directory entry, "loading..." child '''
+        item = QtWidgets.QTreeWidgetItem(parent, [name, ""], 1)
+        loading = QtWidgets.QTreeWidgetItem(item, ["loading...", ""], 1)
+        item.addChild(loading)
+
+    def addTopEntry(self, name, size, directory):
+        item = QtWidgets.QTreeWidgetItem([name, "" if directory else str(size)], 1)
+        self.fileTree.insertTopLevelItem(0, item)
+        if directory:
+            loading = QtWidgets.QTreeWidgetItem(item, ["loading...", ""], 1)
+            item.addChild(loading)
+
+    def addSubEntry(self, parent, name, size, directory):
+        item = QtWidgets.QTreeWidgetItem(parent, [name, "" if directory else str(size)], 1)
+        parent.addChild(item)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
